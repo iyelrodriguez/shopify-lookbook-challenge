22e8bc909adc6100773e16333ff030097eefc8c0
Gil <iyelvillanuevarodriguez@gmail.com>
2026-07-31 01:15:10 +0800

    Implemented Shopify Metaobject lookbook integration

---
+++ shopify-theme/assets/lookbook.js
@@ -0,0 +1,15 @@
+const lookbookContainer = document.getElementById("lookbook-products");
+
+if (lookbookContainer) {
+
+    const productHandles =
+        lookbookContainer.dataset.productHandles
+        .split(",");
+
+    console.log("Product Handles Array:");
+
+    console.log(productHandles);
+
+}
+
+console.log("asdasddd");
